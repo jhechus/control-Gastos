707eccff7c0699e44415296ad2a54e80434521db
jhechus <jess.osm1412@gmail.com>
2023-02-08 17:09:56 -0600

    ajustando el modal a la pantalla cuando haya muchos gastos y poniendo las imagenes

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,6 +1,25 @@
 import React from 'react'
 import { formatearFecha } from '../helpers';
 
+import IconoAhorro from '../img/Icono_ahorro.svg'
+import IconoCasa from '../img/Icono_casa.svg'
+import IconoComida from '../img/Icono_comida.svg'
+import IconoGastos from '../img/Icono_gastos.svg'
+import IconoOcio from '../img/Icono_ocio.svg'
+import IconoSalud from '../img/Icono_salud.svg'
+import IconoSuscripciones from '../img/Icono_suscripciones.svg'
+
+const diccionarioIconos = {
+  ahorro       : IconoAhorro,
+  comida       : IconoComida,
+  casa         : IconoCasa,
+  gastos       : IconoGastos,
+  ocio         : IconoOcio,
+  salud        : IconoSalud,
+  suscripciones: IconoSuscripciones
+}
+
+
 function Gasto({gasto}) {
 
     const { categoria, nombre, cantidad, id, fecha } = gasto;
@@ -8,7 +27,10 @@ function Gasto({gasto}) {
   return (
     <div className=' gasto sombra'>
       <div className=' contenido-gasto'>
-        {/* imagen */}
+        <img 
+          src={diccionarioIconos[categoria]}
+          alt="Icono gasto" 
+        />
         <div className=' descripcion-gasto'>
             <p className=' categoria'> {categoria} </p>
             <p className=' nombre-gasto'> {nombre} </p>
